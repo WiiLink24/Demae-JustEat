6ba81e867579aadf3fbba8b3e865067ef29ed1c9
Noah Pistilli <noah@wiilink.ca>
2026-03-25 13:00:10 -0400

    feat: Add some German categories

--- justeat/categories.go
+++ justeat/categories.go
@@ -3,13 +3,13 @@ package justeat
 import "github.com/WiiLink24/DemaeJustEat/demae"
 
 var categoryTypes = map[demae.CategoryCode][]string{
-	demae.Pizza:            {"pizza"},
-	demae.Western:          {"hamburger", "pollo", "americano", "messicano", "american", "sandwiches"},
-	demae.FastFood:         {"panini", "hamburger", "friti", "pollo", "chicken"},
-	demae.Chinese:          {"cinese", "asianfusion", "asian", "chinese"},
-	demae.DrinksAndDessert: {"dolci", "gelato", "bevande", "desserts", "coffee", "cakes", "drinks"},
-	demae.Curry:            {"curry", "indian"},
+	demae.Pizza:            {"pizza", "italian-style-pizza"},
+	demae.Western:          {"hamburger", "pollo", "americano", "messicano", "american", "sandwiches", "burger", "amerikanisches-essen"},
+	demae.FastFood:         {"panini", "hamburger", "friti", "pollo", "chicken", "burger", "gefluegelgerichte", "doener"},
+	demae.Chinese:          {"cinese", "asianfusion", "asian", "chinese", "asiatisch-essen"},
+	demae.DrinksAndDessert: {"dolci", "gelato", "bevande", "desserts", "coffee", "cakes", "drinks", "getraenke-und-snacks", "snacks"},
+	demae.Curry:            {"curry", "indian", "indian-food"},
 	demae.Japanese:         {"asianfusion", "ramen", "japanese"},
-	demae.PartyFood:        {"kebab"},
+	demae.PartyFood:        {"kebab", "doener"},
 	demae.Sushi:            {"sushi"},
 }
