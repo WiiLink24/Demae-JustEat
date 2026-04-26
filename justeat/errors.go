816350829e87901b16ef054fe188dd054c14cab1
Noah Pistilli <noah@wiilink.ca>
2026-04-26 12:13:43 -0400

    feat: Add guide link to ErrNotLinked

--- justeat/errors.go
+++ justeat/errors.go
@@ -5,6 +5,6 @@ import (
 )
 
 var (
-	ErrNotLinked      = demae.NewSentryError("Please link your Just Eat Account with\nyour WiiLink Account", false)
+	ErrNotLinked      = demae.NewSentryError("Please link your Just Eat Account with\nyour WiiLink Account. Follow the guide\nat https://wiilink.ca/guide/just-eat", false)
 	ErrInvalidCountry = demae.NewSentryError("Your Wii's country does not support\nJust Eat", false)
 )
