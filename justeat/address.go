5d3dca4654b62f947103cbe3bafe0215a6be284c
Harry Walker <contact@harrywalker.uk>
2026-09-01 21:12:57 +0000

    fix: bring back just eat geocoder As has been observed since making the jump to Nominatim, some addresses do not geocode using it either. This reintroduces the official Just Eat geocoder as the primary method for geocoding, using Nominatim as a fallback. This has been tested to work with addresses that only work with one geocoder but not the other on either side.

--- justeat/address.go
+++ justeat/address.go
@@ -2,11 +2,14 @@ package justeat
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/url"
 	"strconv"
 	"time"
 
+	"github.com/WiiLink24/DemaeJustEat/logger"
 	"golang.org/x/time/rate"
 )
 
@@ -24,10 +27,56 @@ type NominatimResponse struct {
 var nominatimLimiter = rate.NewLimiter(rate.Every(time.Second), 1)
 
 func (j *JEClient) getGeocodedAddress() (long float64, lat float64, city string, e error) {
-	err := nominatimLimiter.Wait(j.Context)
+	payload := map[string]any{
+		"addressLines": []string{
+			j.Address,
+			"",
+			"",
+			j.PostalCode,
+		},
+	}
+
+	_url := fmt.Sprintf("%s/geocode/%s", j.KongAPIURL, j.Country)
+	resp, err := j.httpPost(_url, payload)
 	if err != nil {
 		return 0, 0, "", err
 	}
+
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			logger.Error(Address, err.Error())
+		}
+	}(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, 0, "", err
+	}
+
+	// Retrieve longitude and latitude
+	var data map[string]any
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return 0, 0, "", err
+	}
+
+	if data["errors"] != nil {
+		if data["errors"].([]any)[0].(map[string]any)["description"].(string) == "Address cannot be geocoded" {
+			// Unable to be found via Just Eat geocoder, we move onto Nominatim
+			return j.getGeocodedAddressNominatim()
+		}
+
+		return 0, 0, "", errors.New(data["errors"].([]any)[0].(map[string]any)["description"].(string))
+	}
+
+	long = data["geometry"].(map[string]any)["coordinates"].([]any)[0].(float64)
+	lat = data["geometry"].(map[string]any)["coordinates"].([]any)[1].(float64)
+	city = data["properties"].(map[string]any)["addressLineMapping"].(map[string]any)["city"].(string)
+	return
+}
+
+func (j *JEClient) getGeocodedAddressNominatim() (long float64, lat float64, city string, e error) {
+	var err error
 
 	street := url.QueryEscape(j.Address)
 	postalCode := url.QueryEscape(j.PostalCode)
@@ -40,6 +89,11 @@ func (j *JEClient) getGeocodedAddress() (long float64, lat float64, city string,
 			return 0, 0, "", err
 		}
 	} else {
+		err := nominatimLimiter.Wait(j.Context)
+		if err != nil {
+			return 0, 0, "", err
+		}
+
 		url := fmt.Sprintf("https://nominatim.openstreetmap.org/search?street=%s&postalcode=%s&format=json&limit=1&addressdetails=1", street, postalCode)
 		body, err := HttpGet(url)
 		if err != nil {
@@ -60,7 +114,7 @@ func (j *JEClient) getGeocodedAddress() (long float64, lat float64, city string,
 	}
 
 	if len(results) == 0 {
-		return 0, 0, "", fmt.Errorf("no location found")
+		return 0, 0, "", fmt.Errorf("failed to find address - make sure it is set correctly")
 	}
 
 	long, err = strconv.ParseFloat(results[0].Lon, 64)
